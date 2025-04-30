e715599f75884c7e4f2c9a33bb44e72dccb89d1c
eunshil.jeon <eunshil.jeon@gmail.com>
2025-04-30 16:20:18 +0900

    getSum() complete

--- Calc/Calc/main.cpp
+++ Calc/Calc/main.cpp
@@ -3,11 +3,18 @@
 class Cal {
 public:
 	// 이곳에 코드작성
+	int getSum(int a, int b) { return a + b; }
 };
 
 // 테스트 케이스 작성
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, addtest) {
+	int actual = getSum(1, 2);
+	int expected = 3;
+	EXPECT_EQ(actual, expected);
 }
 
 
