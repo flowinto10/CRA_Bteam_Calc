6ed7b0742e2dde4757ace49fbe541d45cad0401c
youngjo park <anyways5188@gmail.com>
2025-04-30 17:10:38 +0900

    build error 및 일부 깨진 문자 제거

--- Calc/Calc/main.cpp
+++ Calc/Calc/main.cpp
@@ -3,12 +3,7 @@
 
 class Cal {
 public:
-
-
 	// 이곳에 코드작성
-
-	// 이곳에 코드작성
-
 	int Zegop(int a) { return a * a; }
 	int getGop(int a, int b) {
 		return a * b;
@@ -17,7 +12,7 @@ public:
   
 	int getDivide(int a, int b) {
 		if (b == 0) {
-			std::cout << "0쇰   듬\n";
+			std::cout << "0 modify comments \n";
 			return -1;
 		}
 		return a / b;
@@ -25,31 +20,28 @@ public:
 
 };
 
-
-// ㅽ 耳댁 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
 }
-// 테스트 케이스 작성
+
 TEST(CalcTC, Zegop1) {
 	Cal cal;
 	int a = 4;
 	int expect = a * a;
 
 	EXPECT_EQ(cal.Zegop(a), expect);
-
 }
-
 
 TEST(CalTest, getGopTest) {
 	Cal cal;
-	
-	EXPECT_EQ( cal.getGop(1,1), 1);
+
+	EXPECT_EQ(cal.getGop(1, 1), 1);
 	EXPECT_EQ(cal.getGop(1, -1), -1);
 	EXPECT_EQ(cal.getGop(-1, -1), 1);
 	EXPECT_EQ(cal.getGop(0, 0), 0);
 	EXPECT_EQ(cal.getGop(0, 1), 0);
-	EXPECT_EQ(cal.getGop(456, 123), 456*123);
+	EXPECT_EQ(cal.getGop(456, 123), 456 * 123);
+}
   
   TEST(t1, dividetest) {
 	Cal cal = Cal();
@@ -63,3 +55,4 @@ int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+
