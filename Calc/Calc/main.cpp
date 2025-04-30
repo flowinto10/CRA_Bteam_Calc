053f0baa3270db11aa6099f1d7675eb34e304959
youngjo park <anyways5188@gmail.com>
2025-04-30 16:12:48 +0900

    first commit

---
+++ Calc/Calc/main.cpp
@@ -0,0 +1,17 @@
+#include "gmock/gmock.h"
+
+class Cal {
+public:
+	// 이곳에 코드작성
+};
+
+// 테스트 케이스 작성
+TEST(t1, t2) {
+	EXPECT_EQ(1, 1);
+}
+
+
+int main() {
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
