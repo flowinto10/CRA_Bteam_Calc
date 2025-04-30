224f6a862ea06b5e2a9be35d9e5a8ec023c5ab7b
youngjo park <anyways5188@gmail.com>
2025-04-30 16:23:53 +0900

    제곱 기능 추가

--- Calc/Calc/main.cpp
+++ Calc/Calc/main.cpp
@@ -3,11 +3,16 @@
 class Cal {
 public:
 	// 이곳에 코드작성
+	int Zegop(int a) { return a * a; }
 };
 
 // 테스트 케이스 작성
-TEST(t1, t2) {
-	EXPECT_EQ(1, 1);
+TEST(CalcTC, Zegop1) {
+	Cal cal;
+	int a = 4;
+	int expect = a * a;
+
+	EXPECT_EQ(cal.Zegop(a), expect);
 }
 
 
